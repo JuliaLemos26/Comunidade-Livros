7c449f9bc3b4eb725946abd9c0c3863c497384cb
julialemos26 <tgpsi23julialemos@etap.pt>
2025-02-18 13:01:04 +0000

    Primeiro Commit

---
+++ ComunidadeLivros.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using ComunidadeLivros.Shared.Services;
+
+namespace ComunidadeLivros.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
---
+++ ComunidadeLivros/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ComunidadeLivros
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
